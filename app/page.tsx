1f518796e39692bba7c030d7c243a5b26228783d
GakuKajiwara <g.k0321aaauuu@gmail.com>
2024-12-10 00:51:39 +0900

    5章まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,43 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
-import ButtonLink from "@/app/_compotents/ButtonLink";
-const name = "世界";
-export default function Home() {
+import { getNewsList } from "@/app/_libs/microcms";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
+import NewsList from "@/app/_components/NewsList";
+import ButtonLink from "@/app/_components/ButtonLink";
+
+export const revalidate = 60;
+
+export default async function Home() {
+  const data = await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  });
   return (
-    <section>
-      <div>
-        <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
-        <p className={styles.description}>
-          私たちは市場をリードしているグローバルテックカンパニーです
-        </p>
-      </div>
-      <Image
-        className={styles.bgimg}
-        src="/img-mv.jpg"
-        alt=""
-        width={4000}
-        height={1200}
-      />
-      <div className="styles.newsLink">
-        <ButtonLink href="/news">もっとみる</ButtonLink>
-      </div>
-    </section>
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <p className={styles.description}>
+            私たちは市場をリードしているグローバルテックカンパニーです。
+          </p>
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+          priority
+          sizes="100vw"
+        />
+      </section>
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={data.contents} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっとみる</ButtonLink>
+        </div>
+      </section>
+    </>
   );
 }
